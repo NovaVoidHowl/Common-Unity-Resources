5ad95f5b033597ffc3d3a793d79e2968685939d6
NovaVoidHowl <me@novavoidhowl.com>
2023-10-01 20:12:16 +0100

    initial commit

---
+++ Editor/coreUI.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace uk.novavoidhowl.dev.common.ui
+{
+  public class coreUI : Editor
+  {
+    //////////////////////////////////////////
+    //// Functions to render common UI elements
+    ////
+    
+    // Function to render a horizontal separator
+    public void renderHorizontalSeparator()
+    {
+      GUILayout.Space(4);
+      GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(2));
+      GUILayout.Space(4);
+    }
+      
+    // render toggle button that switches the state of the boolean passed in, and if bool is true makes the button green, if false makes it red. also takes 2 srings for the text to display when true and false
+    public void renderToggleButton(ref bool boolToToggle, string trueText, string falseText)
+    {
+      if (boolToToggle)
+      {
+        GUI.backgroundColor = Color.green;
+      }
+      else
+      {
+        GUI.backgroundColor = Color.red;
+      }
+
+      if (GUILayout.Button(boolToToggle ? trueText : falseText))
+      {
+        boolToToggle = !boolToToggle;
+      }
+
+      GUI.backgroundColor = Color.white;
+    }
+
+    private void renderFoldoutStart()
+    {
+      EditorGUI.indentLevel++;
+      EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+      GUILayout.Space(EditorGUI.indentLevel * 16);
+      EditorGUILayout.BeginVertical();
+      EditorGUI.indentLevel++;
+      GUILayout.Space(4);
+    }
+
+    private void renderFoldoutEnd()
+    {
+      GUILayout.Space(10);
+      EditorGUI.indentLevel--;
+      EditorGUILayout.EndVertical();
+      EditorGUILayout.EndHorizontal();
+      EditorGUI.indentLevel--;
+      GUI.backgroundColor = Color.white; // prevent bleed through from previous GUI.backgroundColor
+    }
+
+
+    public void renderFoldoutSection(string title, ref bool showSection, Action content, Color foldoutTitleBackgroundColor)
+    {
+      Rect foldoutRect = GUILayoutUtility.GetRect(
+        GUIContent.none,
+        EditorStyles.helpBox,
+        GUILayout.ExpandWidth(true),
+        GUILayout.Height(30f)
+      );
+
+      // Draw background
+      GUIStyle bannerStyle = new GUIStyle(GUI.skin.box);
+      bannerStyle.normal.background = Texture2D.whiteTexture;
+      bannerStyle.normal.textColor = foldoutTitleBackgroundColor;
+      GUI.backgroundColor = foldoutTitleBackgroundColor;
+      GUI.Box(foldoutRect, GUIContent.none, bannerStyle);
+      GUI.backgroundColor = Color.white;
+
+      GUI.DrawTexture(
+        new Rect(foldoutRect.x + 4f, foldoutRect.y + 7f, 16f, 16f),
+        (
+          showSection
+            ? EditorGUIUtility.IconContent("IN Foldout on").image
+            : EditorGUIUtility.IconContent("IN Foldout").image
+        )
+      );
+      Rect labelRect = new Rect(foldoutRect.x + 20f, foldoutRect.y + 7f, foldoutRect.width - 20f, 16f);
+      showSection = GUI.Toggle(labelRect, showSection, title, EditorStyles.boldLabel);
+      if (Event.current.type == EventType.MouseDown && foldoutRect.Contains(Event.current.mousePosition))
+      {
+        showSection = !showSection;
+        Event.current.Use();
+      }
+
+      if (showSection)
+      {
+        renderFoldoutStart();
+
+        GUI.backgroundColor = Color.white;
+        content.Invoke();
+        GUI.backgroundColor = foldoutTitleBackgroundColor;
+
+        renderFoldoutEnd();
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# NVH Common Components
+
+## Script Overview
+
+This repo contains common components that I use in my unity packages
+
+## How to install this package
+
+1> In unity open the `Package Manager` window
+
+2> Click the plus button in the top left of that window and choose the `Add packages from git URL` option
+
+![image](https://github.com/NovaVoidHowl/Mesh-Bone-Rebind/assets/31048789/66eaec96-322e-46ac-811d-353f8209198c)
+
+3> Paste in the git url of this repo ``
+
+![image](https://github.com/NovaVoidHowl/Mesh-Bone-Rebind/assets/31048789/de07970b-7649-4789-aa7b-fc2a00622551)
+
+4> Click the add button
+
+The script should then be ready to use.
+
+## How to use
+
+There is no direct use of this repo, as its a dependency for others
